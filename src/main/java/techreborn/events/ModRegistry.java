b67c91d06fdcedbc41df8a275cf22f93c8f80d92
Promeign <thomas@stieger.ch>
2024-03-17 15:05:25 +0100

    Added Villagers from version 5.2.0

--- src/main/java/techreborn/events/ModRegistry.java
+++ src/main/java/techreborn/events/ModRegistry.java
@@ -63,6 +63,8 @@ public class ModRegistry {
 		registerItems();
 		registerFluids();
 		registerSounds();
+		TRVillager.registerVillagerTrades();
+		TRVillager.registerWanderingTraderTrades();
 	}
 
 	private static void registerBlocks() {
